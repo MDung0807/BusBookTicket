6efb835b493248eb521a9c7e1fa3aec961fce7c9
MDung0807 <77951587+MDung0807@users.noreply.github.com>
2023-10-13 22:29:21 +0700

    Revert "Dung"

--- BusBookTicket.BusStationManage/DTOs/Requests/BST_FormCreate.cs
+++
@@ -1,9 +0,0 @@
-namespace BusBookTicket.BusStationManage.DTOs.Requests;
-
-public class BST_FormCreate
-{
-    public string? name { get; set; }
-    public string? address { get; set; }
-    public string? description { get; set; }
-    public int status { get; set; }
-}
--- BusBookTicket.Company/Repositories/ICompanyRepos.cs
+++
@@ -1,9 +0,0 @@
-using BusBookTicket.Common.Common;
-using BusBookTicket.Common.Models.Entity;
-
-namespace BusBookTicket.CompanyManage.Repositories;
-
-public interface ICompanyRepos : IRepository<Company>
-{
-    
-}
