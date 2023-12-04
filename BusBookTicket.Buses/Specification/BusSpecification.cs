0595a6a94d7c044af42cfa5d184cbc21b25f70c4
MDung0807 <dominhdung21082002@gmail.com>
2023-12-04 12:32:28 +0700

    fix: only change information for bus in company

--- BusBookTicket.Buses/Specification/BusSpecification.cs
+++ BusBookTicket.Buses/Specification/BusSpecification.cs
@@ -4,7 +4,7 @@ using Mailjet.Client.Resources;
 
 namespace BusBookTicket.Buses.Specification;
 
-public sealed class BusSpecification: BaseSpecification<Bus>
+public sealed class BusSpecification : BaseSpecification<Bus>
 {
     public BusSpecification(int id, bool checkStatus = true) : base(x => x.Id == id, checkStatus: checkStatus)
     {
@@ -12,9 +12,10 @@ public sealed class BusSpecification: BaseSpecification<Bus>
         AddInclude(x => x.BusStops);
         AddInclude(x => x.BusType);
         AddInclude(x => x.Seats);
-    
-        
-    public BusSpecification(int id, int idCompany, bool checkStatus = true) 
+    }
+
+
+public BusSpecification(int id, int idCompany, bool checkStatus = true) 
         : base(x => x.Id == id 
                     && x.Company.Id == idCompany,
             checkStatus: checkStatus)
