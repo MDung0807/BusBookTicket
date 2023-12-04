70e1adbb83cc610e46ce20a8f933e7c44f82aba2
MDung0807 <dominhdung21082002@gmail.com>
2023-12-04 11:30:11 +0700

    add total seat in bus response

--- BusBookTicket.Buses/DTOs/Responses/BusResponse.cs
+++ BusBookTicket.Buses/DTOs/Responses/BusResponse.cs
@@ -5,7 +5,9 @@ public class BusResponse
     public int Id { get; set; }
     public string BusNumber { get; set; }
     public string BusType { get; set; }
+    public int TotalSeat { get; set; }
+    public string Description { get; set; }
     public List<string> BusStops { get; set; }
-    public int Status;
+    public int Status { get; set; }
     public string Company { get; set; }
 }
